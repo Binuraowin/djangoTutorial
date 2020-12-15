a4055893c4d6945c844560b98002b09d23bb5428
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-12-15 12:21:26 +0530

    static html

--- djangoTest/djangoTest/urls.py
+++ djangoTest/djangoTest/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('homee/', include('calc.urls')),
+path('traveller/', include('newspapaer.urls')),
     path('admin/', admin.site.urls),
 ]

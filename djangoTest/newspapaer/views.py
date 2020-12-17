2d99a40dfe9b76aea1549d9ef6defcce56813564
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-12-18 02:23:50 +0530

    dynamic html

--- djangoTest/newspapaer/views.py
+++ djangoTest/newspapaer/views.py
@@ -2,8 +2,41 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from .models import Destination
+
 
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+
+    dest1 = Destination()
+    dest1.desc="binura woin destination"
+    dest1.name="binura"
+    dest1.img="destination_1.jpg"
+    dest1.price=700
+    dest1.offer=True
+
+    dest2 = Destination()
+    dest2.desc = "binura woin destination"
+    dest2.name = "binura"
+    dest2.img = "destination_1.jpg"
+    dest2.price = 700
+    dest2.offer = False
+
+    dest3 = Destination()
+    dest3.desc = "binura woin destination"
+    dest3.name = "binura"
+    dest3.img = "destination_2.jpg"
+    dest3.price = 700
+    dest3.offer = False
+
+    dest4 = Destination()
+    dest4.desc = "binura woin destination"
+    dest4.name = "binura"
+    dest4.img = "destination_3.jpg"
+    dest4.price = 700
+    dest4.offer = True
+
+    destList = [dest1,dest2,dest3,dest4]
+
+    return render(request,"index.html",{"destList":destList})

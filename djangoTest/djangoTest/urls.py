2296e1ad6992eefab385b3f88b9efc35be5a75c7
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-12-19 14:35:15 +0530

    auth

--- djangoTest/djangoTest/urls.py
+++ djangoTest/djangoTest/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('homee/', include('calc.urls')),
-path('traveller/', include('newspapaer.urls')),
+    path('traveller/', include('newspapaer.urls')),
     path('admin/', admin.site.urls),
+    path('traveller/accounts/',include('accounts.urls'))
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangoTest/newspapaer/views.py
+++ djangoTest/newspapaer/views.py
@@ -8,35 +8,6 @@ from .models import Destination
 # Create your views here.
 
 def index(request):
-
-    dest1 = Destination()
-    dest1.desc="binura woin destination"
-    dest1.name="binura"
-    dest1.img="destination_1.jpg"
-    dest1.price=700
-    dest1.offer=True
-
-    dest2 = Destination()
-    dest2.desc = "binura woin destination"
-    dest2.name = "binura"
-    dest2.img = "destination_1.jpg"
-    dest2.price = 700
-    dest2.offer = False
-
-    dest3 = Destination()
-    dest3.desc = "binura woin destination"
-    dest3.name = "binura"
-    dest3.img = "destination_2.jpg"
-    dest3.price = 700
-    dest3.offer = False
-
-    dest4 = Destination()
-    dest4.desc = "binura woin destination"
-    dest4.name = "binura"
-    dest4.img = "destination_3.jpg"
-    dest4.price = 700
-    dest4.offer = True
-
-    destList = [dest1,dest2,dest3,dest4]
+    destList = Destination.objects.all()
 
     return render(request,"index.html",{"destList":destList})
